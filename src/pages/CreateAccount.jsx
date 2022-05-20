6fdc4323747a13d38f368e295d189ab7a2be43ee
Luis Ariza <luismanuelariza@hotmail.com>
2022-05-20 11:43:54 -0500

    Englobando estilos

--- src/pages/CreateAccount.jsx
+++ src/pages/CreateAccount.jsx
@@ -5,19 +5,19 @@ const CreateAccount = () => {
 	return (
 
 	<main id="main">
-		<section className="create-account">
-			<section className="create-account__container">
+		<section className="login-section create-account">
+			<section className="login-section__container create-account__container">
 				<h1 className="create-account__title">My account</h1>
 				<form action="/" className="form create-account__form">
 					{/* Nombre */}
-					<label htmlFor="name">Name</label>
-					<input type="text" id="name" placeholder="Camilo Perez" className="input create-account__input--name" />
+					<label htmlFor="name" className="label">Name</label>
+						<input type="text" id="name" placeholder="Camilo Perez" className="input create-account__input--name" />
 					{/* Email */}
-					<label htmlFor="email">Email ad	dress</label>
-					<input type="text" id="email" placeholder="email@example.com" className="input create-account__input--email" />
+					<label htmlFor="email" className="label">Email address</label>
+						<input type="text" id="email" placeholder="email@example.com" className="input create-account__input--email" />
 					{/* Contraseña */}
-					<label htmlFor="password">Password</label>
-					<input type="password" id="password" placeholder="************" className="input create-account__password" />
+					<label htmlFor="password" className="label">Password</label>
+						<input type="password" id="password" placeholder="************" className="input create-account__input--password" />
 				</form>
 			</section>
 			<button className="primary-button create-account__button">Create account</button>
